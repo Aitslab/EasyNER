721a9647ede95a69b692c90714607e2d17bdb81a
rafsanahmed <rafsan.ahmed@std.antalya.edu.tr>
2023-04-17 13:36:37 +0200

    data fix

--- scripts/search.py
+++ scripts/search.py
@@ -3,36 +3,79 @@
 
 import json
 import os
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from glob import glob
-import utils
+import util
 
 class EntitySearch:
 
-    def __init__(self, input_folder, entities):
+    def __init__(self, input_folder, output_file, entities):
         
         self.input_folder = input_folder
+        self.output_file = output_file
         self.entities = entities
         
         
-    def load_files(self, self.input_folder):
+    def sort_files(self, input_folder):
         
         return sorted(glob(f'{input_folder}*.json'), key=lambda x: int(os.path.splitext(os.path.basename(x))[0].split("-")[-1]))
         
-    def search(input_files)
+    def read_files(self, input_file):
+        
+        with open(input_file, encoding="utf8") as f:
+            articles = json.loads(f.read())
+        
+        return articles
+        
+    def search(self, input_files_list, entities):
+        
+        '''
+        search entities within a file
 
+        '''
+        
+        main_dict = {}
+        for idx, input_file in tqdm(enumerate(input_files_list)):
+        
+            articles = self.read_files(input_file)
+            
+            for art, val in tqdm(articles.items()):
+                print(art, val)
+                for sent in val["sentences"]:
+                    if len(sent["entities"])==0:
+                        continue
+                    else:
+        #                 print(sent["entities"])
+                        for entity in entities:
+                            if entity in sent["entities"]:
+                                if art not in main_dict:
+                                    main_dict[art]={"sentences":[]}
+                                print(sent["entities"])
+                                main_dict[art]["sentences"].append({"text":sent["text"], "entities": sent["entities"], "entity_spans": sent["entity_spans"]})
+        
+        return main_dict
+        
+    
+    def run(self):
+    
+        input_files_list = self.sort_files(self.input_folder)
+        
+        main_dict = self.search(input_files_list, self.entities)
+        
+        util.append_to_json_file(self.output_file, main_dict)
+        
         
 
 
 if __name__ == "__main__":
     
-    model_dir = "../../rafsan/models/biobert_pytorch_pretrained/"
-    model_name = "HunFlair_chemical_all/"
-    seq = "he is feeing very sick nitrous oxide NO nucleus eucaryotic A549 HeLa Cells oxygen."
+    input_folder = "../../NER_pipeline/results_testeval_p50/text-ner-mtorandtsc1_cell/"
+    output_file = "../../NER_pipeline/results_testeval_p50/search/text-search-mtorandtsc1_cell.json"
+    entities = ["tsc", "mtor", "cell", "cells"]
     
-    NER = NER_biobert(model_dir=model_dir, model_name=model_name)
-    
-    print(NER.predict(seq))
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    searcher = EntitySearch(input_folder, output_file, entities)
+    searcher.run()
     
     
 
