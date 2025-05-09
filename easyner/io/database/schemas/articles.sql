b874813847154fd04e1a8c9fdd14af0146cc3133
Carl Ollvik Aasa <75130322+callebalik@users.noreply.github.com>
2025-05-09 17:58:50 +0200

    fix(database): remove NOT NULL constraints and default values from articles table columns

--- easyner/io/database/schemas/articles.sql
+++ easyner/io/database/schemas/articles.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS articles (
     article_id INTEGER PRIMARY KEY,
-    title VARCHAR NOT NULL,
-    text TEXT DEFAULT NULL,
-    metadata TEXT DEFAULT NULL,
+    title VARCHAR,
+    text TEXT,
+    metadata TEXT
 );
