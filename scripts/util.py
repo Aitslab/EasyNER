2898927f4466607f2edf8e89c8bdb89ac041e942
Sonja Aits <32497093+SonjaAits@users.noreply.github.com>
2025-05-15 11:49:26 +0200

    change to orjson for performance

--- scripts/util.py
+++ scripts/util.py
@@ -1,19 +1,24 @@
+"""
+Author: Sonja Aits (partially based in previous script by Rafsan Ahmed)
+
+"""
+
 # coding=utf-8
 
-import json
+import orjson
 import os
 
 
 def append_to_json_file(path: str, new_data: dict):
     if not os.path.isfile(path):
-        with open(path, "w", encoding="utf-8") as f:
-            f.write("{}")
+        with open(path, "wb") as f:  # Use binary mode for orjson
+            f.write(orjson.dumps({}))
 
-    with open(path, "r", encoding="utf-8") as f:
-        old_data = json.loads(f.read())
+    with open(path, "rb") as f:  # Binary read
+        old_data = orjson.loads(f.read())
 
     data = {**old_data, **new_data}  # Merge dicts (new overwrites old)
 
     with open(path, "w", encoding="utf-8") as f:
-        f.write(json.dumps(data, indent=2, ensure_ascii=False))
+        f.write(orjson.dumps(data, option=orjson.OPT_INDENT_2))
         
